b9e43af00120e6e3f381901ab84b6040a946cb86
Constantin St <dinscalin.ks@gmail.com>
2022-09-21 21:10:11 +0700

    refactor: complite getShipping

--- src/main/java/org/example/Logistics.java
+++ src/main/java/org/example/Logistics.java
@@ -1,21 +1,27 @@
 package org.example;
 
 public class Logistics {
-		private Transport[] vehicles;
+	private Transport[] vehicles;
     public Logistics(Transport ... vehicles) {
 		this.vehicles = vehicles;
 		}
-	public Class<? extends Transport> getShipping(City city, int weight, int hours) {
-		getVehicles();
-		for(int i = vehicles.length-1; i>=0; i--) {
-			if(vehicles[i].getCapacity() <= weight ) {
+	public Transport getShipping(City city, int weight, int hours) {
+		int j = 0;
+		for(int i = vehicles.length-1; i >= 0; i--) {
+			if(vehicles[i].getCapacity() >= weight ) {
 				if(city.getDistanceKm()/vehicles[i].getSpeed() <= hours) {
-					System.out.println(vehicles[i].getClass());
-					return vehicles[i].getClass();
+					if(vehicles[i].getPrice(city) < vehicles[0].getPrice(city)) {
+							j = i;
+					}
 				}
 			}
-		} return null;
+		} return vehicles[j];
 	}
+	private boolean  isShippingAvailable() {
+
+		return true;
+	}
+
 
 	public Transport[] getVehicles() {
 		return vehicles;
