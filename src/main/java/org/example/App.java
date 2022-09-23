041096ac8be455e8c53c55b7b8c6742f349d25ba
ConstantinGSt <stepanyuk.kon@yandex.ru>
2022-09-23 16:08:44 +0700

    rrrr

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,33 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        City gonduras = new City("Gonduras", 100,true,false);
+        City sosedsk = new City("Sosedsk", 15);
+        City stoliciya = new City("BigSity", 578, true,true);
+
+        Truck chetverka = new Truck("Kolimaga", 1, 19, 4.0f);
+        Truck kamaz = new Truck("Kamazz", 25, 90, 30f);
+        Plane an2 = new Plane("An-2", 3,180, 250);
+        Plane tu = new Plane("Tu-154", 18,900, 1000);
+        Ship sailBoat = new Ship("Black Pearl", 685, 20, 100);
+        Ship seiner = new Ship("Angara", 160,24,200);
+        
+        System.out.println("Стоимость колымаги до Гондураса: " + chetverka.getPrice(gonduras));
+        System.out.println("Стоимость камаза до Соседска: " + kamaz.getPrice(sosedsk));
+        System.out.println("Стоимость Ан-2 до Гондураса: " + an2.getPrice(gonduras));
+        System.out.println("Стоимость Tu-154 до Соседска: " + tu.getPrice(sosedsk));
+        System.out.println("Стоимость Black Pearl до BigСity: " + sailBoat.getPrice(stoliciya));
+        System.out.println("Стоимость Angara до Гондураса: " + seiner.getPrice(gonduras));
+        System.out.println("Стоимость Angara до Соседска: " + seiner.getPrice(sosedsk));
+
+        Logistics test = new Logistics(kamaz, chetverka, an2, tu, sailBoat, seiner);
+
+        System.out.printf("вариант Транспорта до города: %s%n", test.getShipping(gonduras, 1, 30));
+
+    }
+}
---
+++ src/main/java/org/example/Ship.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Ship extends Transport {
+
+	public Ship(String name, int capacity, int speed, float costOfKm) {
+		super(name, speed, capacity, costOfKm);
+	}
+	public float getPrice(City city) {
+		if(city.isOnWater() == true) {
+			return (float) (city.getDistanceKm() * getCostOfKm());
+		} else return 0;
+	}
+}
+
