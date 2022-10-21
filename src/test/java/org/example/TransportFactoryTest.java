99c3ec76937f2d3e37fde27d83a62e26a10fddf7
ConstantinGSt <stepanyuk.kon@yandex.ru>
2022-10-21 18:06:54 +0700

    add test

---
+++ src/test/java/org/example/TransportFactoryTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TransportFactoryTest {
+
+	@Test
+	public void getTransport() {
+	}
+}
